dbbea0aea2081feb840aa1b73dbf7c95d1d68103
mohabroha@mail.de <mohabroha@mail.de>
2026-05-13 16:25:10 +0200

    Step 4: implement user registration API with DRF

---
+++ backend/auth_app/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            "message": "User created successfully",
+            "user": serializer.data
+        }, status=status.HTTP_201_CREATED)
